d3d2f5784f69d5dba8370f53aa52cde6ddf151b7
Lexevolution <31176843+Lexevolution@users.noreply.github.com>
2023-11-12 21:29:23 +1000

    Will I finish this? Added check for media attachments

--- index.mjs
+++ index.mjs
@@ -12,6 +12,7 @@ const botClient = new Client({intents: [
 
 botClient.on(Events.MessageCreate, (message) => {
     let announement_type = null;
+    let attachment_ids = [];
     switch (message.channelId){
         case "1171348411589607434": //#resonite-announcements
             announement_type = "announcement";
@@ -28,15 +29,60 @@ botClient.on(Events.MessageCreate, (message) => {
 
     //Do some things here
     if (typeof announement_type === "string"){
+        //Check to see if post has media attachment.
+        if (message.attachments){
+            message.attachments.forEach(async attachment => {
+                fetch(attachment.url).then((res) => {
+                    fetch("https://social.lexevo.net/api/v2/media", {
+                        method: "POST",
+                        headers: {
+                            "Authorization": `Bearer ${process.env.FEDI_TOKEN}`
+                        },
+                        body: await res.formData()
+                    }).then((res2) => {
+                        if (res2.status === 200){
+                            res2.json().then((resJson) => {
+                                attachment_ids.push(resJson.id);
+                            })
+                        }
+                    });
+                });
+            });
+
+        }
+        //Check for rapid succession of messages
+        
+        let bodyBuilder = {
+            "status": message.
+        }
+        
         fetch("https://social.lexevo.net/api/v1/statuses", {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${process.env.FEDI_TOKEN}`,
                 "Content-Type": "application/json"
-            }
+            },
+            body:
         });
     }
     
 });
+
+botClient.on("messageUpdate", (oldMessage, newMessage) => {
+    //Check to see if message was edited
+    switch (newMessage.channelId){
+        case "1171348411589607434": //#resonite-announcements
+            announement_type = "announcement";
+            break;
+        case "1171348449367687239": //#resonite-updates
+            announement_type = "update"
+            break;
+        case "1171348477201088532": //#resonite-devlog
+            announement_type = "devlog";
+            break;
+        default:
+            break;
+    }
+})
 
 botClient.login(process.env.DISCORD_TOKEN);
