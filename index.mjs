334fd1b6988e2ea088b9f2860eb9b7236c102550
Lexevolution <31176843+Lexevolution@users.noreply.github.com>
2023-11-13 21:22:57 +1000

    Trying to figure out media uploads

--- index.mjs
+++ index.mjs
@@ -10,7 +10,7 @@ const botClient = new Client({intents: [
     GatewayIntentBits.GuildMembers
 ]});
 
-botClient.on(Events.MessageCreate, (message) => {
+botClient.on(Events.MessageCreate, async (message) => {
     let announement_type = null;
     let attachment_ids = [];
     switch (message.channelId){
@@ -23,6 +23,9 @@ botClient.on(Events.MessageCreate, (message) => {
         case "1171348477201088532": //#resonite-devlog
             announement_type = "devlog";
             break;
+        case "1173566951319146536": //#test
+            announement_type = "testing";
+            break;
         default:
             break;
     }
@@ -30,15 +33,18 @@ botClient.on(Events.MessageCreate, (message) => {
     //Do some things here
     if (typeof announement_type === "string"){
         //Check to see if post has media attachment.
+        console.log(JSON.stringify(message.attachments));
         if (message.attachments){
-            message.attachments.forEach(async attachment => {
-                fetch(attachment.url).then((res) => {
+            for (const attachment of message.attachments.values()){
+                let data = new FormData();
+                await fetch(attachment.url).then(async (res) => {
+                    data.append("file", await res.blob());
                     fetch("https://social.lexevo.net/api/v2/media", {
                         method: "POST",
                         headers: {
                             "Authorization": `Bearer ${process.env.FEDI_TOKEN}`
                         },
-                        body: await res.formData()
+                        body: data
                     }).then((res2) => {
                         if (res2.status === 200){
                             res2.json().then((resJson) => {
@@ -47,22 +53,25 @@ botClient.on(Events.MessageCreate, (message) => {
                         }
                     });
                 });
-            });
+            }
 
+            console.log("ATTACHMENT IDS: " + JSON.stringify(attachment_ids));
         }
         //Check for rapid succession of messages
         
-        let bodyBuilder = {
-            "status": message.
+        const bodyBuilder = {
+            "status": `Resonite ${announement_type} post: ${message.content}\n\n#resonite`,
+            "media_ids": attachment_ids,
+            "visibility": "private"
         }
-        
+        console.log(JSON.stringify(message.author));
         fetch("https://social.lexevo.net/api/v1/statuses", {
             method: "POST",
             headers: {
                 "Authorization": `Bearer ${process.env.FEDI_TOKEN}`,
                 "Content-Type": "application/json"
             },
-            body:
+            body: JSON.stringify(bodyBuilder)
         });
     }
     
@@ -79,6 +88,9 @@ botClient.on("messageUpdate", (oldMessage, newMessage) => {
             break;
         case "1171348477201088532": //#resonite-devlog
             announement_type = "devlog";
+            break;
+        case "1173566951319146536": //#test
+            announement_type = "testing";
             break;
         default:
             break;
