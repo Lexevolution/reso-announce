257b876b9cfad79e505ef85c9274abf16c9cc40d
Lexevolution <31176843+Lexevolution@users.noreply.github.com>
2023-11-14 21:20:11 +1000

    Things are looking up! - Fix async annoyances - Fix typos

--- index.mjs
+++ index.mjs
@@ -11,48 +11,50 @@ const botClient = new Client({intents: [
 ]});
 
 botClient.on(Events.MessageCreate, async (message) => {
-    let announement_type = null;
+    let announcement_type = null;
     let attachment_ids = [];
     switch (message.channelId){
         case "1171348411589607434": //#resonite-announcements
-            announement_type = "announcement";
+            announcement_type = "announcement";
             break;
         case "1171348449367687239": //#resonite-updates
-            announement_type = "update"
+            announcement_type = "update"
             break;
         case "1171348477201088532": //#resonite-devlog
-            announement_type = "devlog";
+            announcement_type = "devlog";
             break;
         case "1173566951319146536": //#test
-            announement_type = "testing";
+            announcement_type = "testing";
             break;
         default:
             break;
     }
 
     //Do some things here
-    if (typeof announement_type === "string"){
+    if (typeof announcement_type === "string"){
         //Check to see if post has media attachment.
-        console.log(JSON.stringify(message.attachments));
         if (message.attachments){
             for (const attachment of message.attachments.values()){
+                console.log(`DISCORD ATTACHMENTS: ${JSON.stringify(attachment)}`);
                 let data = new FormData();
-                await fetch(attachment.url).then(async (res) => {
-                    data.append("file", await res.blob());
-                    fetch("https://social.lexevo.net/api/v2/media", {
-                        method: "POST",
-                        headers: {
-                            "Authorization": `Bearer ${process.env.FEDI_TOKEN}`
-                        },
-                        body: data
-                    }).then((res2) => {
-                        if (res2.status === 200){
-                            res2.json().then((resJson) => {
-                                attachment_ids.push(resJson.id);
-                            })
-                        }
-                    });
+
+                let res = await fetch(attachment.url);
+                data.append("file", await res.blob());
+
+                let res2 = await fetch("https://social.lexevo.net/api/v2/media", {
+                    method: "POST",
+                    headers: {
+                        "Authorization": `Bearer ${process.env.FEDI_TOKEN}`
+                    },
+                    body: data
                 });
+
+                console.log(`UPLOAD ATTEMPT RESULTS: ${JSON.stringify(res2)}`);
+                if (res2.status === 200){
+                    const resJson = await res2.json();
+                    attachment_ids.push(resJson.id);
+                }
+
             }
 
             console.log("ATTACHMENT IDS: " + JSON.stringify(attachment_ids));
@@ -60,11 +62,11 @@ botClient.on(Events.MessageCreate, async (message) => {
         //Check for rapid succession of messages
         
         const bodyBuilder = {
-            "status": `Resonite ${announement_type} post: ${message.content}\n\n#resonite`,
+            "status": `Resonite ${announcement_type} post: ${message.content}\n\n#resonite`,
             "media_ids": attachment_ids,
             "visibility": "private"
-        }
-        console.log(JSON.stringify(message.author));
+        };
+
         fetch("https://social.lexevo.net/api/v1/statuses", {
             method: "POST",
             headers: {
@@ -78,19 +80,20 @@ botClient.on(Events.MessageCreate, async (message) => {
 });
 
 botClient.on("messageUpdate", (oldMessage, newMessage) => {
+    let announcement_type = null;
     //Check to see if message was edited
     switch (newMessage.channelId){
         case "1171348411589607434": //#resonite-announcements
-            announement_type = "announcement";
+            announcement_type = "announcement";
             break;
         case "1171348449367687239": //#resonite-updates
-            announement_type = "update"
+            announcement_type = "update"
             break;
         case "1171348477201088532": //#resonite-devlog
-            announement_type = "devlog";
+            announcement_type = "devlog";
             break;
         case "1173566951319146536": //#test
-            announement_type = "testing";
+            announcement_type = "testing";
             break;
         default:
             break;
